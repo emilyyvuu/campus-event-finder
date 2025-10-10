c3b86ac6e36263754ca218e249319d7bd44a40c5
Ethan Nguyen <ethannguyen@vt.edu>
2025-10-10 15:18:47 -0400

    Created HomePage.jsx and installed React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import HomePage from '../pages/HomePage.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/home', element: <HomePage /> },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )

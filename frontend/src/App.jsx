62517d93e6c8ba0e9a637daf40fc145ea09d4264
Emily Vu <emily.vu@sapns2.com>
2025-10-12 13:20:04 -0400

    add event details page, navbar component, mock testing data forr events, and react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import EventMainPage from "../pages/EventMainPage"
+import { Routes, Route, Navigate } from "react-router-dom";
+import EventMainPage from "./pages/EventMainPage";
+import EventDetailPage from "./pages/EventDetailsPage";
 
 function App() {
-  return <EventMainPage />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/events" replace />} />
+      <Route path="/events" element={<EventMainPage />} />
+      <Route path="/events/:id" element={<EventDetailPage />} />
+      <Route path="*" element={<div className="container">Not found</div>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;

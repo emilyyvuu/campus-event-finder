9f73e734165c4c64eef1880fe42d828a4f200024
Emily Vu <emily.vu@sapns2.com>
2025-11-05 13:56:30 -0500

    fix api urls for deployment

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,25 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// Define allowed origins
+const allowedOrigins = [
+  "http://localhost:5173", // your React dev URL
+  process.env.FRONTEND_URL, // your deployed frontend URL
+];
+
 // Allow frontend (important for cookies)
 app.use(
   cors({
-    origin: "http://localhost:5173", // your React dev URL
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
